f058d82bc9c5d3c883163895fd7f251629e95c3f
rvcas <x@rvcas.dev>
2025-03-29 23:58:22 -0400

    chore: add dummy snapshot test

--- tests/Test.hs
+++ tests/Test.hs
@@ -1,4 +1,15 @@
 module Main where
 
+import Data.ByteString.Lazy.Char8 qualified as BS
+import Test.Tasty
+import Test.Tasty.Golden
+
 main :: IO ()
-main = putStrLn "Hello, World!"
+main = defaultMain tests
+
+tests :: TestTree
+tests =
+  testGroup
+    "Parser Tests"
+    [ goldenVsString "Dummy Parse" "tests/golden/dummy.txt" $ pure $ BS.pack "Hello, World!"
+    ]
