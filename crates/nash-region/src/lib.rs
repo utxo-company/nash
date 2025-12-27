5b7d3cd1dec9bde1f7c276bd68e141c4e54c39b0
rvcas <x@rvcas.dev>
2025-12-27 01:47:44 -0500

    feat: define expression source ast
    Signed-off-by: rvcas <x@rvcas.dev>

--- crates/nash-region/src/lib.rs
+++ crates/nash-region/src/lib.rs
@@ -1,14 +1,94 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::cmp::Ordering;
+
+#[derive(Clone, Eq, Copy, PartialEq, PartialOrd, Ord, Hash)]
+pub struct Located<T> {
+    region: Region,
+    value: T,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl<T> Located<T> {
+    pub const fn at(region: Region, value: T) -> Located<T> {
+        Located { region, value }
+    }
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    pub const fn at_zero(value: T) -> Located<T> {
+        let region = Region::zero();
+
+        Located { region, value }
+    }
+}
+
+#[derive(Clone, Eq, Copy, PartialEq, PartialOrd, Ord, Hash)]
+pub struct Region {
+    start: Position,
+    end: Position,
+}
+
+impl Region {
+    pub const fn zero() -> Self {
+        Self {
+            start: Position::zero(),
+            end: Position::zero(),
+        }
+    }
+
+    pub const fn one() -> Self {
+        Self {
+            start: Position::one(),
+            end: Position::one(),
+        }
+    }
+
+    pub const fn new(start: Position, end: Position) -> Self {
+        Self { start, end }
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.start == self.end
+    }
+
+    pub fn contains(&self, other: &Self) -> bool {
+        self.start <= other.start && self.end >= other.end
+    }
+
+    pub fn contains_pos(&self, pos: Position) -> bool {
+        self.start <= pos && self.end >= pos
+    }
+
+    pub fn span_across(start: &Region, end: &Region) -> Self {
+        Region {
+            start: start.start,
+            end: end.end,
+        }
+    }
+}
+
+#[derive(Clone, Eq, Copy, PartialEq, Hash)]
+pub struct Position {
+    line: u16,
+    column: u16,
+}
+
+impl Position {
+    pub const fn zero() -> Self {
+        Self { line: 0, column: 0 }
+    }
+
+    pub const fn one() -> Self {
+        Self { line: 1, column: 1 }
+    }
+}
+
+impl PartialOrd for Position {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Position {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.line
+            .cmp(&other.line)
+            .then_with(|| self.column.cmp(&other.column))
     }
 }
--- crates/nash-source/src/lib.rs
+++ crates/nash-source/src/lib.rs
@@ -1,14 +1,80 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use nash_region::{Located, Region};
+
+pub enum Expr<'a> {
+    Str(&'a str),
+    Int(i128),
+    Var(VarType, &'a str),
+    VarQual(VarType, &'a str, &'a str),
+    List(&'a [&'a Located<Expr<'a>>]),
+    Op(&'a str),
+    Negate(&'a Located<Expr<'a>>),
+    BinOps(
+        &'a [(&'a Located<Expr<'a>>, &'a Located<&'a str>)],
+        &'a Located<Expr<'a>>,
+    ),
+    Lambda {
+        parameters: &'a [&'a Located<Pattern<'a>>],
+        body: &'a Located<Expr<'a>>,
+    },
+    Call {
+        function: &'a Located<Expr<'a>>,
+        arguments: &'a [&'a Located<Expr<'a>>],
+    },
+    If {
+        branches: &'a [&'a IfBranch<'a>],
+        final_else: &'a Located<Expr<'a>>,
+    },
+    Let(&'a [&'a Located<Expr<'a>>], &'a Located<Expr<'a>>),
+    Case {
+        scrutinee: &'a Located<Expr<'a>>,
+        arms: &'a [&'a CaseArm<'a>],
+    },
+    Accessor(&'a str),
+    Update(&'a Located<&'a str>, &'a [&'a FieldAssign<'a>]),
+    Record(&'a [&'a FieldAssign<'a>]),
+    Unit,
+    Tuple(
+        &'a Located<Expr<'a>>,
+        &'a Located<Expr<'a>>,
+        &'a [&'a Located<Expr<'a>>],
+    ),
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub enum VarType {
+    LowVar,
+    CapVar,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub struct IfBranch<'a> {
+    pub condition: &'a Located<Expr<'a>>,
+    pub body: &'a Located<Expr<'a>>,
+}
+
+pub struct CaseArm<'a> {
+    pub pattern: &'a Located<Pattern<'a>>,
+    pub body: &'a Located<Expr<'a>>,
+}
+
+pub struct FieldAssign<'a> {
+    pub field: &'a Located<&'a str>,
+    pub value: &'a Located<Expr<'a>>,
+}
+
+pub enum Pattern<'a> {
+    Anything,
+    Var(&'a str),
+    Record(&'a [&'a Located<&'a str>]),
+    Alias(&'a Located<Pattern<'a>>, &'a Located<&'a str>),
+    Unit,
+    Tuple(
+        &'a Located<Pattern<'a>>,
+        &'a Located<Pattern<'a>>,
+        &'a [&'a Located<Pattern<'a>>],
+    ),
+    Ctor(&'a Region, &'a str, &'a [&'a Located<Pattern<'a>>]),
+    CtorQual(&'a Region, &'a str, &'a str, &'a [&'a Located<Pattern<'a>>]),
+    List(&'a [&'a Located<Pattern<'a>>]),
+    Cons(&'a Located<Pattern<'a>>, &'a Located<Pattern<'a>>),
+    Str(&'a str),
+    Int(i128),
 }
