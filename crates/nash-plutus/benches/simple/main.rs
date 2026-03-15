493a3ecc4504bb0fbc8d71851082b22d7d914a79
rvcas <x@rvcas.dev>
2026-03-14 22:20:38 -0400

    chore: delete silly benches

--- crates/nash-plutus/benches/simple/main.rs
+++
@@ -1,8 +0,0 @@
-use criterion::criterion_main;
-
-mod simple;
-
-criterion_main! {
-    simple::add_integer,
-    simple::fibonacci,
-}
--- crates/nash-plutus/benches/simple/simple.rs
+++
@@ -1,6 +0,0 @@
-mod add_integer;
-mod fibonacci;
-mod utils;
-
-pub use add_integer::*;
-pub use fibonacci::*;
